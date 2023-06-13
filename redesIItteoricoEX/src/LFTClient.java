6bbd913b365f298a7711a3b9ecb6d45e0df21e14
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2023-06-13 13:38:56 +0200

    quito el hola

--- redesIItteoricoEX/src/LFTClient.java
+++ redesIItteoricoEX/src/LFTClient.java
@@ -18,8 +18,9 @@ public class LFTClient {
                     break;
                 case 4:
                     if (!args[1].equals("modo=SSL")) {
+                        System.err.println("La sintaxis empleada para la activación del SSL es incorrecta. En <modo> emplear \"modo=SSL\" para proceder con su activación");
                         // ! ERROR + log
-                        System.exit(-1);
+                        System.exit(1);
                     } else {
                         modoSSL = true;
                         host = args[2];
@@ -28,14 +29,16 @@ public class LFTClient {
                     }
                     break;
                 default:
+                System.err.println("Los argumentos introducidos es incorrecto. Uso: <modo> <host> <puerto> <carpeta_cliente>");
                     // ! ERROR + log
-                    System.exit(-2);
+                    System.exit(2);
                     break;
             }
         } catch (Exception e) {
             // ! ERROR + log
-
+            
         }
+        System.out.println("Se han recogido los argumentos correctamente. <modoSSL="+modoSSL+"> <host="+host+"> <puerto="+puerto+"> <carpeta_cliente="+carpetaCliente+">");
         // * log los argumentos
         LFTClient _miCliente = new LFTClient();
         // _miCliente.start(puerto);
